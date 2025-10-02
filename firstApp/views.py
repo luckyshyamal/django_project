1805d85e9a3972aaa4780c774e98989ee3305d00
luckyshyamal <boirishyamal2906@gmail.com>
2025-10-02 22:29:19 +0530

    first django project comment

---
+++ firstApp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def firstapp(request):
+    return HttpResponse("This is my first app")
+
+def htmlfile(request):
+    return render(request, 'index.html')
