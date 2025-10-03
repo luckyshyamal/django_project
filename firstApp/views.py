51360de6a81bb8542cd4646a1456f5d9ce563dc6
luckyshyamal <boirishyamal2906@gmail.com>
2025-10-03 20:24:08 +0530

    hello world

--- firstApp/views.py
+++ firstApp/views.py
@@ -7,3 +7,6 @@ def firstapp(request):
 
 def htmlfile(request):
     return render(request, 'index.html')
+
+def htmlfile2(request):
+    return render(request, 'index2.html')
--- firstproject/urls.py
+++ firstproject/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', firstapp),
     path('htmlfile/',htmlfile),
+    path('htmlfile2/', htmlfile2),
 ]
